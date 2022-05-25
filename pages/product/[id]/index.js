dc922cdbc65ac7f2898fe44af58beaab950edf52
nguyenthoai@eplaza.vn <nguyenthoai@eplaza.vn>
2022-05-25 16:14:51 +0700

    update

--- pages/product/[id]/index.js
+++ pages/product/[id]/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function index() {
-  return <div>tesssss</div>;
+  return (
+    <div>
+      <Head>
+        <title>Thoại đz</title>
+      </Head>
+    </div>
+  );
 }
 
 export default index;
