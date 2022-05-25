30d315265c8399070c6c9b24c347bc5fdf79e5aa
nguyenthoai@eplaza.vn <nguyenthoai@eplaza.vn>
2022-05-25 16:30:31 +0700

    update

--- pages/product/[id]/index.js
+++ pages/product/[id]/index.js
@@ -8,6 +8,15 @@ function index() {
         <title>My page title</title>
         <meta property="og:title" content="My page title" key="title" />
         <meta name="description" content={"hello"} />
+        <meta
+          property="og:url"
+          content="https://cdnimg.vietnamplus.vn/uploaded/xtsqr/2022_03_18/iphone_13_1.jpg"
+        />
+        <meta property="og:description" content="đây là des" />
+        <meta
+          property="og:image"
+          content="https://cdnimg.vietnamplus.vn/uploaded/xtsqr/2022_03_18/iphone_13_1.jpg"
+        />
         {/* <meta
           itemprop="image"
           content="https://cheatcode-assets.s3.amazonaws.com/default-social-graphic.png"
